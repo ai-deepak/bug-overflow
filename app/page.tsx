fc63fc3c5568db207a78a378c163c3970f23b1ba
Deepak Ravikumar <deepak.060593@gmail.com>
2025-04-04 16:59:51 +0530

    first-commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+const Home = () => (
+  <h1 className="mx-auto mt-10 max-w-sm rounded-lg border border-gray-300 bg-gray-100 p-6 text-center text-lg font-semibold leading-relaxed tracking-wide text-gray-700 shadow-lg">
+    Welcome to the world of nextjs
+  </h1>
+);
+
+export default Home;
