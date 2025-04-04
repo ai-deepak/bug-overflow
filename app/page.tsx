753d1a64f20afeaadc26a7507d50e7a6840fa50a
Deepak Ravikumar <deepak.060593@gmail.com>
2025-04-04 17:19:06 +0530

    tailwind-theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,3 @@
-const Home = () => (
-  <h1 className="mx-auto mt-10 max-w-sm rounded-lg border border-gray-300 bg-gray-100 p-6 text-center text-lg font-semibold leading-relaxed tracking-wide text-gray-700 shadow-lg">
-    Welcome to the world of nextjs
-  </h1>
-);
+const Home = () => <h1 className="h1-bold">Welcome to the world of nextjs</h1>;
 
 export default Home;
